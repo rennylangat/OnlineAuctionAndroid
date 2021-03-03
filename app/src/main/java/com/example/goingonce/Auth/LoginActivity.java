c441b14d4bf8b6273a30ad1e410b2f5a4650448c
RennyLangat <rennylngt@gmail.com>
2021-03-04 02:39:21 +0300

    Screens

---
+++ app/src/main/java/com/example/goingonce/Auth/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.goingonce.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.goingonce.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
