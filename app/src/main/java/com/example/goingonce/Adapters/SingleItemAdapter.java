ecb81297913f158bcd9fada76c9145d7eecbeb86
RennyLangat <rennylngt@gmail.com>
2021-03-04 16:55:47 +0300

    Adapters

--- app/src/main/java/com/example/goingonce/Adapters/SingleItemAdapter.java
+++ app/src/main/java/com/example/goingonce/Adapters/SingleItemAdapter.java
@@ -1,4 +1,44 @@
 package com.example.goingonce.Adapters;
 
-public class SingleItemAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.goingonce.models.ItemDets;
+
+import java.util.ArrayList;
+
+public class SingleItemAdapter extends RecyclerView.Adapter {
+    private String mDataset[];
+    private Context mContext;
+    private ArrayList<ItemDets> itemDets;
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        LinearLayout view;
+        
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/goingonce/models/ItemDets.java
+++ app/src/main/java/com/example/goingonce/models/ItemDets.java
@@ -1,4 +1,57 @@
 package com.example.goingonce.models;
 
 public class ItemDets {
+    String itemName;
+    String itemPrice;
+    String itemDesc;
+    String itemImg;
+
+    public ItemDets(String itemName, String itemPrice, String itemDesc, String itemImg) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemDesc = itemDesc;
+        this.itemImg = itemImg;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(String itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemDesc() {
+        return itemDesc;
+    }
+
+    public void setItemDesc(String itemDesc) {
+        this.itemDesc = itemDesc;
+    }
+
+    public String getItemImg() {
+        return itemImg;
+    }
+
+    public void setItemImg(String itemImg) {
+        this.itemImg = itemImg;
+    }
+
+    @Override
+    public String toString() {
+        return "ItemDets{" +
+                "itemName='" + itemName + '\'' +
+                ", itemPrice='" + itemPrice + '\'' +
+                ", itemDesc='" + itemDesc + '\'' +
+                ", itemImg='" + itemImg + '\'' +
+                '}';
+    }
 }
