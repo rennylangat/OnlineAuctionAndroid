abdeb3fbdc4dd194b8daed932daa95e90c763458
RennyLangat <rennylngt@gmail.com>
2021-03-07 17:51:56 +0300

    Major Changes

--- app/src/main/java/com/example/goingonce/activities/AddPostActivity.java
+++ app/src/main/java/com/example/goingonce/activities/AddPostActivity.java
@@ -1,14 +1,14 @@
-package com.example.goingonce;
+package com.example.goingonce.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AddPostActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_add_post);
     }
 }
---
+++ app/src/main/java/com/example/goingonce/activities/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.goingonce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.example.goingonce.Adapters.SingleItemAdapter;
+import com.example.goingonce.models.ItemDets;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter recyclerAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private SingleItemAdapter singleItemAdapter;
+    private ArrayList<ItemDets> itemDets;
+    private Context mContext=MainActivity.this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpWidgets();
+    }
+
+    private void setUpWidgets() {
+        recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setAdapter(recyclerAdapter);
+        itemDets=new ArrayList<ItemDets>();
+
+    }
+}
