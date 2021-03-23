49b256d274d785d8365bfa716ff4ad6264938032
RennyLangat <rennylngt@gmail.com>
2021-03-23 23:58:03 +0300

    Material Design

--- app/src/main/java/com/example/goingonce/activities/AddPostActivity.java
+++ app/src/main/java/com/example/goingonce/activities/AddPostActivity.java
@@ -2,16 +2,24 @@ package com.example.goingonce.activities;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -25,14 +33,16 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 public class AddPostActivity extends AppCompatActivity {
 
     private ImageButton imgBtn;
     private EditText itmName,itmDesc,itmBaseBid,itmStartTime,itmEndTime;
+    private AutoCompleteTextView itemType;
     private Button submitBtn;
-
+    private Context mContext=AddPostActivity.this;
     private Uri imgUri;
     private StorageReference mStorageRef;
     private DatabaseReference mRef;
@@ -40,18 +50,42 @@ public class AddPostActivity extends AppCompatActivity {
     private static final int GALLERY_REQUEST=1;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
 
+        getWindow().setStatusBarColor(Color.BLACK);
+
         imgBtn=(ImageButton)findViewById(R.id.imgBtn);
         itmName=findViewById(R.id.edit_item_name);
         itmDesc=findViewById(R.id.edit_desc);
         itmBaseBid=findViewById(R.id.edit_base_bid);
-        itmStartTime=findViewById(R.id.edit_start_time);
+        itmStartTime=findViewById(R.id.edit_location);
         itmEndTime=findViewById(R.id.edit_end_time);
         submitBtn=findViewById(R.id.submitBtn);
+        itemType=findViewById(R.id.edit_item_type);
+
+        ArrayList<String> typesList=getTypesList();
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(mContext,android.R.layout.simple_spinner_dropdown_item,typesList);
+
+        itemType.setAdapter(adapter);
+
+        itemType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu=new PopupMenu(mContext,itemType);
+                popupMenu.inflate(R.menu.type_menu);
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        itemType.setText(item.getTitle());
+                        return true;
+                    }
+                });
+            }
+        });
 
 
         itmStartTime.setOnClickListener(new View.OnClickListener() {
@@ -112,6 +146,17 @@ public class AddPostActivity extends AppCompatActivity {
                 publishPost(itemName,itemDesc,baseBid,startTime,endTime);
             }
         });
+    }
+
+    private ArrayList<String> getTypesList() {
+        ArrayList<String> types=new ArrayList<>();
+        types.add("Electronics");
+        types.add("Cosmetics");
+        types.add("Jewellery");
+        types.add("Clothing");
+        types.add("Cars");
+        types.add("Property");
+        return  types;
     }
 
     @Override
